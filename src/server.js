b27128d20b2e600ad2c116abd8fbff0b5a8cb074
Collid3 <masolathabangjerry11@gmail.com>
2025-04-24 13:43:16 +0200

    added production url to cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://chat-server-e9hl.onrender.com"],
     credentials: true,
   })
 );
